796745a4ca851bca90283341c9d8eaed75225bbd
Aravind Zennialpro <aravind.puttapaka@thezennialpro.com>
2025-10-28 15:27:01 +0530

    Added Personal_Travel_Assistant(PTA) - FastAPI - MongoDB

--- Projects/Personal_Travel_Assistant(PTA)/Personal_Travel_Assistant(PTA)/app/main.py
+++ Projects/Personal_Travel_Assistant(PTA)/Personal_Travel_Assistant(PTA)/app/main.py
@@ -4,13 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Personal Travel Assistant")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(search_router, prefix="/api")
 
 @app.get("/")
